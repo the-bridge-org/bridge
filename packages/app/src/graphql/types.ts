6c643242169dc01f8915b865ee0fd2111be6e387
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-11-17 09:48:36 -0800

    Add login resolver function

--- packages/app/src/graphql/types.ts
+++ packages/app/src/graphql/types.ts
@@ -1,40 +1,49 @@
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type LoginInput = {
+  username: Scalars["String"];
+  password: Scalars["String"];
 };
 
 export type Mutation = {
-   __typename?: 'Mutation',
-  register: User,
+  __typename?: "Mutation";
+  register: User;
+  login: User;
 };
 
-
 export type MutationRegisterArgs = {
-  payload: RegisterInput
+  payload: RegisterInput;
+};
+
+export type MutationLoginArgs = {
+  payload: LoginInput;
 };
 
 export type Query = {
-   __typename?: 'Query',
-  ping: Scalars['String'],
+  __typename?: "Query";
+  ping: Scalars["String"];
 };
 
 export type RegisterInput = {
-  username: Scalars['String'],
-  phoneNumber: Scalars['String'],
-  password: Scalars['String'],
+  username: Scalars["String"];
+  phoneNumber: Scalars["String"];
+  password: Scalars["String"];
 };
 
 export type User = {
-   __typename?: 'User',
-  id: Scalars['ID'],
-  username: Scalars['String'],
-  phoneNumber: Scalars['String'],
+  __typename?: "User";
+  id: Scalars["ID"];
+  username: Scalars["String"];
+  phoneNumber: Scalars["String"];
   /** Only register, login, and me will return access token */
-  token?: Maybe<Scalars['String']>,
-  createdAt: Scalars['Int'],
+  token?: Maybe<Scalars["String"]>;
+  createdAt: Scalars["Int"];
 };
